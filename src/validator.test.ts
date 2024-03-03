dba9765d00ea7253b012ec0200351e5277f24324
Eldar <elderblr2@gmail.com>
2024-03-03 16:39:41 +0100

    improve validator tests

--- src/validator.test.ts
+++ src/validator.test.ts
@@ -1,10 +1,17 @@
 import { validateWordsType } from './validator';
 
 describe("Validator", () => {
-    test("Should throw error if words have incorrect type", () => {
-        const errorMessage = "Provided words are incorrect, please use String type";
+    describe("insertIntoArray", () => {
+        test("Should throw error if words have incorrect type", () => {
+            const errorMessage = "Provided words are incorrect, please use String type";
 
-        expect(() => validateWordsType(null, 'word')).toThrow(errorMessage);
-        expect(() => validateWordsType('some', undefined)).toThrow(errorMessage);
+            expect(() => validateWordsType(null, 'word')).toThrow(errorMessage);
+            expect(() => validateWordsType('some', undefined)).toThrow(errorMessage);
+        });
+
+        test("Should return True using correct params", () => {
+            const result = validateWordsType("word1", 'word2');
+            expect(result).toBe(true);
+        });
     });
 });
--- src/validator.ts
+++ src/validator.ts
@@ -2,4 +2,6 @@ export const validateWordsType = (word1: string, word2: string) => {
     if (word1 == null || word2 == null) {
         throw new Error("Provided words are incorrect, please use String type");
     }
+
+    return true;
 }
